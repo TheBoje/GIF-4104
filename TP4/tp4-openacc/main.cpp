7b6b6c144e806bda87c1eaf5e20d31c12903910f
Vincent Commin <Altimors@users.noreply.github.com>
2022-04-03 15:08:52 -0400

    test openacc

--- TP4/tp4-openacc/main.cpp
+++ TP4/tp4-openacc/main.cpp
@@ -1,19 +1,45 @@
 #include <iostream>
 #include <chrono>
 
-#define N 1000000000
+#include "Matrix.hpp"
+
+#define N 1000
+
+// Multiplier deux matrices.
+Matrix multiplyMatrix(const Matrix& iMat1, const Matrix& iMat2) {
+	assert(iMat1.cols() == iMat2.rows());
+
+	Matrix lRes(iMat1.rows(), iMat2.cols());
+    
+    // #pragma acc parallel
+    {
+        // #pragma acc loop
+        for (size_t i = 0; i < lRes.rows(); ++i) {
+            // #pragma acc loop
+            for (size_t j = 0; j < lRes.cols(); ++j) {
+                lRes(i, j) = (iMat1.getRowCopy(i) * iMat2.getColumnCopy(j)).sum();
+            }
+        }
+    }
+
+	return lRes;
+}
 
 int main(int argc, char * argv[]) {
 
-    int *a = (int *)malloc(sizeof(int) * N);
+    Matrix mat1(N, N);
+    Matrix mat2(N, N);
+
+    mat1 = MatrixRandom(N, N);
+    mat2 = MatrixRandom(N, N);
 
     auto start = std::chrono::steady_clock::now();
 
-    #pragma acc parallel loop copyout(a)
-    for(int i = 0; i < N; i++) 
-        a[i] = 2*i;
+    Matrix res = multiplyMatrix(mat1, mat2);
 
     auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double> elapsed_seconds = end-start;
     std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+
+    // std::cout << "res = \n" << res.str() << std::endl;
 }
