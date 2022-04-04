fa2175c0397bffa6acb7d82dc1cd9c3177f14f17
TheBoje <TheBoje@users.noreply.github.com>
2022-04-04 19:40:49 -0400

    fix: stdcerr for openacc time

--- TP4/tp4-openacc/main.cpp
+++ TP4/tp4-openacc/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <chrono>
+#include <iostream>
 
 #include "Matrix.hpp"
 
@@ -12,83 +12,78 @@ void invertSequential(Matrix& iA) {
 	// construire la matrice [A I]
 	MatrixConcatCols lAI(iA, MatrixIdentity(iA.rows()));
 
-	// traiter chaque rangée
-    #pragma acc data copy(lAI)
-    {
+// traiter chaque rangée
+#pragma acc data copy(lAI)
+	{
+		for (size_t k = 0; k < iA.rows(); ++k) {
+			// trouver l'index p du plus grand pivot de la colonne k en valeur absolue
+			// (pour une meilleure stabilité numérique).
+			size_t p	= k;
+			double lMax = fabs(lAI(k, k));
+			for (size_t i = k; i < lAI.rows(); ++i) {
+				if (fabs(lAI(i, k)) > lMax) {
+					lMax = fabs(lAI(i, k));
+					p	 = i;
+				}
+			}
 
-        for (size_t k = 0; k < iA.rows(); ++k) {
-            // trouver l'index p du plus grand pivot de la colonne k en valeur absolue
-            // (pour une meilleure stabilité numérique).
-            size_t p	= k;
-            double lMax = fabs(lAI(k, k));
-            for (size_t i = k; i < lAI.rows(); ++i) {
-                if (fabs(lAI(i, k)) > lMax) {
-                    lMax = fabs(lAI(i, k));
-                    p	 = i;
-                }
-            }
+			// vérifier que la matrice n'est pas singulière
+			if (lAI(p, k) == 0)
+				throw std::runtime_error("Matrix not invertible");
 
-            // vérifier que la matrice n'est pas singulière
-            if (lAI(p, k) == 0)
-                throw std::runtime_error("Matrix not invertible");
+			// échanger la ligne courante avec celle du pivot
+			if (p != k)
+				lAI.swapRows(p, k);
 
-            // échanger la ligne courante avec celle du pivot
-            if (p != k)
-                lAI.swapRows(p, k);
+			double lValue = lAI(k, k);
 
-            double lValue = lAI(k, k);
+#pragma acc parallel loop
+			for (size_t j = 0; j < lAI.cols(); ++j) {
+				// On divise les éléments de la rangée k
+				// par la valeur du pivot.
+				// Ainsi, lAI(k,k) deviendra égal à 1.
+				lAI(k, j) /= lValue;
+			}
 
-            #pragma acc parallel loop 
-            for (size_t j = 0; j < lAI.cols(); ++j) {
-                // On divise les éléments de la rangée k
-                // par la valeur du pivot.
-                // Ainsi, lAI(k,k) deviendra égal à 1.
-                lAI(k, j) /= lValue;
-            }
-
-            // Pour chaque rangée...
-            #pragma acc parallel loop copyin(k)
-            for (size_t i = 0; i < lAI.rows(); ++i) {
-                if (i != k) { // ...différente de k
-                    // On soustrait la rangée k
-                    // multipliée par l'élément k de la rangée courante
-                    double lValue = lAI(i, k);
-                    lAI.getRowSlice(i) -= lAI.getRowCopy(k) * lValue;
-                }
-            }
-        }
-    }
-
-	// On copie la partie droite de la matrice AI ainsi transformée
-	// dans la matrice courante (this).
-    #pragma acc parallel loop
-	for (unsigned int i = 0; i < iA.rows(); ++i) {
-		iA.getRowSlice(i) = lAI.getDataArray()[std::slice(i * lAI.cols() + iA.cols(), iA.cols(), 1)];
+// Pour chaque rangée...
+#pragma acc parallel loop copyin(k)
+			for (size_t i = 0; i < lAI.rows(); ++i) {
+				if (i != k) { // ...différente de k
+					// On soustrait la rangée k
+					// multipliée par l'élément k de la rangée courante
+					double lValue = lAI(i, k);
+					lAI.getRowSlice(i) -= lAI.getRowCopy(k) * lValue;
+				}
+			}
+		}
 	}
-}
 
+// On copie la partie droite de la matrice AI ainsi transformée
+// dans la matrice courante (this).
+#pragma acc parallel	  loop
+	 for (unsigned int i = 0; i < iA.rows(); ++i) {
+		 iA.getRowSlice(i) = lAI.getDataArray()[std::slice(i * lAI.cols() + iA.cols(), iA.cols(), 1)];
+	 }
+}
 
 // Multiplier deux matrices.
 Matrix multiplyMatrix(const Matrix& iMat1, const Matrix& iMat2) {
 	assert(iMat1.cols() == iMat2.rows());
 
 	Matrix lRes(iMat1.rows(), iMat2.cols());
-    
-    #pragma acc parallel loop
-    for (size_t i = 0; i < lRes.rows(); ++i) {
-        #pragma acc loop
-        for (size_t j = 0; j < lRes.cols(); ++j) {
-            lRes(i, j) = (iMat1.getRowCopy(i) * iMat2.getColumnCopy(j)).sum();
-        }
-    }
+
+#pragma acc parallel loop
+	for (size_t i = 0; i < lRes.rows(); ++i) {
+#pragma acc loop
+		for (size_t j = 0; j < lRes.cols(); ++j) {
+			lRes(i, j) = (iMat1.getRowCopy(i) * iMat2.getColumnCopy(j)).sum();
+		}
+	}
 
 	return lRes;
 }
 
-int main(int argc, char * argv[]) {
-
-
-
+int main(int argc, char* argv[]) {
 	srand((unsigned)time(NULL));
 
 	unsigned int lMatSize;
@@ -104,28 +99,27 @@ int main(int argc, char * argv[]) {
 
 	double lTStart, lTEnd;
 
-    lA = MatrixRandom(lMatSize, lMatSize);
-    lB = lA;
+	lA = MatrixRandom(lMatSize, lMatSize);
+	lB = lA;
 
 	// invertParallel(lB);
 
-    auto start = std::chrono::steady_clock::now();
+	auto start = std::chrono::steady_clock::now();
 	invertSequential(lB);
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end-start;
+	auto						  end			  = std::chrono::steady_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
 
-    auto start_mult = std::chrono::steady_clock::now();
-    Matrix lDot = multiplyMatrix(lA, lB);
-    auto end_mult = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_seconds_mult = end_mult - start_mult;
+	auto						  start_mult		   = std::chrono::steady_clock::now();
+	Matrix						  lDot				   = multiplyMatrix(lA, lB);
+	auto						  end_mult			   = std::chrono::steady_clock::now();
+	std::chrono::duration<double> elapsed_seconds_mult = end_mult - start_mult;
 
-    std::cout << "Matrix size: " << lA.cols() << std::endl;
-    std::cout << "Error: " << lDot.getDataArray().sum() - lMatSize << std::endl;
-    // std::cout << lA.str() << std::endl << std::endl;
-    // std::cout << lB.str() << std::endl << std::endl;
-    // std::cout << multiplyMatrix(lA, lB).str() << std::endl << std::endl;
+	std::cout << "Matrix size: " << lA.cols() << std::endl;
+	std::cout << "Error: " << lDot.getDataArray().sum() - lMatSize << std::endl;
+	// std::cout << lA.str() << std::endl << std::endl;
+	// std::cout << lB.str() << std::endl << std::endl;
+	// std::cout << multiplyMatrix(lA, lB).str() << std::endl << std::endl;
 
-    std::cout << "elapsed time: " << elapsed_seconds.count() << "s and elapsed time multiplication: " << elapsed_seconds_mult.count() << "s\n";
-
+	std::cerr << elapsed_seconds.count() << std::endl;
 	return 0;
 }
