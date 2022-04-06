f5076dd6f0e808dad51a4208aabc093d2c8180ff
TheBoje <TheBoje@users.noreply.github.com>
2022-04-06 10:28:56 -0400

    add: finished rapport TP4

--- TP4/tp4-openacc/bench.sh
+++ TP4/tp4-openacc/bench.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+make clean;
 make;
 
 echo  "mat_size,time";
 
-for size in {16,32,64,128,256,512,1024,2048,4096}; do
+for size in {16,32,64,128,256,512,1024,2048}; do
     echo -e "${size},\c" && ./main ${size} > /dev/null;
 done
--- TP4/tp4-opencl/bench.sh
+++ TP4/tp4-opencl/bench.sh
@@ -4,6 +4,6 @@ make;
 
 echo  "mat_size,time";
 
-for size in {16,32,64,128,256,512,1024,2048}; do
+for size in {16,32,64,128}; do
     echo -e "${size},\c" && ./main ${size} > /dev/null;
 done
