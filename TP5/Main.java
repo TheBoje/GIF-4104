2fe0eb45207e0a1c576373f8274756f9e2e34c0b
TheBoje <TheBoje@users.noreply.github.com>
2022-04-13 09:46:30 -0400

    fix: main & makefile, add: bench

--- TP5/Main.java
+++ TP5/Main.java
@@ -1,26 +1,25 @@
+
 /**
     Autors: Vincent COMMIN & Louis LEENART
  */
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-
-import scala.Tuple2;
-
 import com.google.common.collect.Iterables;
-
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.log4j.*;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
-    PageRank algorithm applied to URLs
-
-    Inspired by: https://en.wikipedia.org/wiki/PageRank
+ * PageRank algorithm applied to URLs
+ * Inspired by: https://en.wikipedia.org/wiki/PageRank
  */
 public final class Main {
     private static class Sum implements Function2<Double, Double, Double> {
@@ -30,27 +29,35 @@ public final class Main {
         }
     }
 
-    private final static int INTERATIONS = 100;
+    private final static int ITERATIONS = 100;
     private final static double DAMPING_FACTOR = 0.85;
 
     public static void main(String[] args) throws Exception {
-        if (args.length < 1) {
-            System.out.println("usage: java -cp ... Main <json_path>");
+        if (args.length < 2) {
+            System.out.println("Missing arguments, recieved " + args.length + ", expected 2");
+            System.out.println("usage: Main <json_path> <master>");
+            System.out.println("\tjson_path:    relative path to JSON input file");
+            System.out.println("\tmaster:       spark master address");
             return;
         }
+        String fileName = "";
+        String master = "";
+        fileName = args[0];
+        master = args[1];
+        System.out.println(args.length + " " + master + " " + fileName);
 
-        // Spark default log level is INFO, which is not convinent for this
+        // Spark default log level is INFO, which is not convenient for this
         // Project, so we set it to WARN only.
         Logger rootLogger = Logger.getRootLogger();
         rootLogger.setLevel(Level.WARN);
         Logger.getLogger("org.apache.spark").setLevel(Level.WARN);
         Logger.getLogger("org.spark-project").setLevel(Level.WARN);
 
-        // Setup Spark session
-        SparkSession spark = SparkSession.builder().master("local[*]").appName("PageRankTP5").getOrCreate();
+        // Setup Spark
+        SparkSession spark = SparkSession.builder().master(master).appName("PageRankTP5").getOrCreate();
 
         // Loads all data from json
-        JavaRDD<Row> jsonRows = spark.read().json(args[0]).javaRDD();
+        JavaRDD<Row> jsonRows = spark.read().json(fileName).javaRDD();
         JavaPairRDD<Long, Iterable<Long>> links = jsonRows.mapToPair(s -> {
             Long urlID = s.getLong(s.fieldIndex("id"));
             List<Long> neighborsIDs = s.getList(s.fieldIndex("neighbors"));
@@ -64,7 +71,7 @@ public final class Main {
         JavaPairRDD<Long, Double> ranks = links.mapValues(rs -> 1.0);
 
         // Calculates and updates ranks
-        for (int current = 0; current < INTERATIONS; current++) {
+        for (int current = 0; current < ITERATIONS; current++) {
             // Calculates URL contributions to the rank of other URLs
             JavaPairRDD<Long, Double> contribs = links.join(ranks).values()
                     .flatMapToPair(s -> {
@@ -80,11 +87,14 @@ public final class Main {
             ranks = contribs.reduceByKey(new Sum()).mapValues(sum -> 0.15 + sum * DAMPING_FACTOR);
         }
 
-        // Stop time benchmark
-        long elapsedTime = System.nanoTime() - startTime;
-
         // Print results
         List<Tuple2<Long, Double>> output = new ArrayList<Tuple2<Long, Double>>(ranks.collect());
+
+        // Stop time benchmark
+        // As spark is performing lazy operations, we need to wait after the collect.
+        // Sorting & IO ops aren't part of the benchmark.
+        long elapsedTime = System.nanoTime() - startTime;
+
         Comparator<Tuple2<Long, Double>> myComparator = new Comparator<Tuple2<Long, Double>>() {
             public int compare(Tuple2<Long, Double> t1, Tuple2<Long, Double> t2) {
                 return Double.compare(t2._2(), t1._2());
@@ -97,7 +107,8 @@ public final class Main {
             System.out.println(rows.get(tuple._1().intValue()).getString(3) + " : " + tuple._2() + ".");
         }
 
+        spark.sqlContext().clearCache(); // Or it'll mess benchmark results
         spark.stop();
-        System.err.println((double)elapsedTime / 1_000_000_000.0 + " sec");
+        System.err.println((double) elapsedTime / 1_000_000_000.0 + " sec");
     }
 }
